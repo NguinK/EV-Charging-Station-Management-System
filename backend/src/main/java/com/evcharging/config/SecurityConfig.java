809cbc034d182a81dba311b9839041e2b6627314
Tran Le Khoi Nguyen <tlkn2240@gmail.com>
2025-10-02 08:40:36 +0700

    config swagger

--- backend/src/main/java/com/evcharging/config/SecurityConfig.java
+++ backend/src/main/java/com/evcharging/config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package com.evcharging.config;
 
 
+//import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -19,10 +24,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/auth/**").permitAll() // mở toàn bộ auth
+                        .requestMatchers("/admin/auth/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // mở toàn bộ auth
                         .anyRequest().authenticated()
-                );
-
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
