696a5133977f1effd532e5010798b7ee8aacab45
Tran Le Khoi Nguyen <tlkn2240@gmail.com>
2025-10-15 21:30:40 +0700

    update WebConfig

--- backend/src/main/java/com/evcharging/config/WebConfig.java
+++ backend/src/main/java/com/evcharging/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //cho phép tất cả endpoint
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); //true nếu dùng cookie
