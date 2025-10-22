8a607b7cdbdadf041bfc878496270b3c0f0b5046
MTr2504 <bahugome@gmail.com>
2025-10-22 13:27:19 +0700

    Create ConnectorType.java
    Enums cho Connect

---
+++ backend/src/main/java/com/evcharging/enums/ConnectorType.java
@@ -0,0 +1,9 @@
+package com.evcharging.enums;
+
+// Enums
+public enum ConnectorType {
+    CCS,        // Combined Charging System
+    CHADEMO,    // CHAdeMO
+    AC_TYPE2,   // AC Type 2 (Mennekes)
+    TESLA       // Tesla Supercharger
+}
