d43335973084e95b9f6d147d110abd6d1828b009
MTr2504 <bahugome@gmail.com>
2025-10-22 13:27:51 +0700

    Create PointStatus.java
    Characterictics cho Point

---
+++ backend/src/main/java/com/evcharging/enums/PointStatus.java
@@ -0,0 +1,9 @@
+package com.evcharging.enums;
+
+public enum PointStatus {
+    AVAILABLE,   // Sẵn sàng sử dụng
+    OCCUPIED,    // Đang được sử dụng
+    OFFLINE,     // Không hoạt động
+    RESERVED,    // Đã được đặt trước
+    MAINTENANCE  // Đang bảo trì
+}
