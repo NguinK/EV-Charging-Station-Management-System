aba15bb04676f704e0b9b797d27be33e717d9d79
MTr2504 <bahugome@gmail.com>
2025-10-23 21:48:02 +0700

    Create ChargingPriceType.java
    các loại giá sạc

---
+++ backend/src/main/java/com/evcharging/enums/ChargingPriceType.java
@@ -0,0 +1,8 @@
+package com.evcharging.enums;
+
+public enum ChargingPriceType {
+    BY_KWH,         // Tính theo kWh
+    BY_TIME,        // Tính theo thời gian (phút)
+    MIXED,          // Kết hợp cả 2
+    SUBSCRIPTION    // Theo gói đăng ký
+}
