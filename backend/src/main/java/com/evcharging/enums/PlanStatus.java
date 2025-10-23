34526c01a6c6ad3e62919c4f56d99e0c80af71b7
MTr2504 <bahugome@gmail.com>
2025-10-23 21:48:43 +0700

    Create PlanStatus.java
    trạng thái dịch vụ

---
+++ backend/src/main/java/com/evcharging/enums/PlanStatus.java
@@ -0,0 +1,7 @@
+package com.evcharging.enums;
+
+public enum PlanStatus {
+    ACTIVE,      // Gói đang hoạt động
+    INACTIVE,    // Gói tạm ngưng
+    ARCHIVED     // Gói đã lưu trữ (không còn cung cấp)
+}
