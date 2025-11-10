d9acc3dda758ba7cdc8672464bc4bc990282795e
Tran Le Khoi Nguyen <tlkn2240@gmail.com>
2025-11-10 11:21:06 +0700

    Delete PointStatus

--- backend/src/main/java/com/evcharging/enums/PointStatus.java
+++
@@ -1,9 +0,0 @@
-package com.evcharging.enums;
-
-public enum PointStatus {
-    AVAILABLE,   // Sẵn sàng sử dụng
-    OCCUPIED,    // Đang được sử dụng
-    OFFLINE,     // Không hoạt động
-    RESERVED,    // Đã được đặt trước
-    MAINTENANCE  // Đang bảo trì
-}
