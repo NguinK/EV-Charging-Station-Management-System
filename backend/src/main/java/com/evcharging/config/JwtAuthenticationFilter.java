ac7efc985a468f1d415bbb5818c16b480a5c2cb5
Tran Le Khoi Nguyen <tlkn2240@gmail.com>
2025-11-15 00:39:59 +0700

    Update Jwt

--- backend/src/main/java/com/evcharging/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/evcharging/config/JwtAuthenticationFilter.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Configuration
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
--- backend/src/main/java/com/evcharging/config/JwtUtil.java
+++ backend/src/main/java/com/evcharging/config/JwtUtil.java
@@ -3,12 +3,12 @@ package com.evcharging.config;
 import com.evcharging.enums.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
 
@@ -16,47 +16,36 @@ import java.util.Date;
 public class JwtUtil {
 
     // Tạo key từ chuỗi bí mật
-    private final Key SECRET_KEY = Keys.hmacShaKeyFor(
-            "u8G0z7h1Qm9Zt6yJp3X4n8a2d9F5r6c7d8e9f0g1h2i3j4k5l6m7n8o9p0q1r2s".getBytes()
+    private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
+            "u8G0z7h1Qm9Zt6yJp3X4n8a2d9F5r6c7d8e9f0g1h2i3j4k5l6m7n8o9p0q1r2s".getBytes(StandardCharsets.UTF_8)
     );
+
     private final long EXPIRATION_TIME = 86_400_000;
 
     public String generateToken(String email, Role role) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
         return Jwts.builder()
-                .setSubject(email)
-                .claim("role", role.name())   // thêm role vào claim
-                .setIssuedAt(now)
-                .setExpiration(expiryDate)
-                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .subject(email)
+                .claim("role","ROLE_"+ role.name())   // thêm role vào claim
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
     public String extractUsername(String token) {
-        Claims claims = Jwts.parser()              // trả về JwtParserBuilder
-                .setSigningKey(SECRET_KEY)        // truyền key
-                .build()                          // build parser
-                .parseClaimsJws(token)            // parse token
-                .getBody();                       // lấy claims
+        Claims claims = extractAllClaims(token);
         return claims.getSubject();
     }
 
     public String extractRole(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = extractAllClaims(token);
         return claims.get("role", String.class);
     }
 
     public Instant getExpirationFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = extractAllClaims(token);
         return claims.getExpiration().toInstant();
     }
 
@@ -67,6 +56,14 @@ public class JwtUtil {
 
     public boolean validateToken(String token, UserDetails userDetails) {
         String username = extractUsername(token);
-        return username.equals(userDetails.getUsername());
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
