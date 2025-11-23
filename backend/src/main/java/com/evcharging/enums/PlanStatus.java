40c6ed852d6e38092fabdd1dad30d549a34e7512
LocNPD <nguyenphungdailoc@gmail.com>
2025-11-23 14:19:31 +0700

    Delete PlanStatus

--- backend/src/main/java/com/evcharging/enums/PlanStatus.java
+++
@@ -1,7 +0,0 @@
-package com.evcharging.enums;
-
-public enum PlanStatus {
-    ACTIVE,      // Gói đang hoạt động
-    INACTIVE,    // Gói tạm ngưng
-    ARCHIVED     // Gói đã lưu trữ (không còn cung cấp)
-}
